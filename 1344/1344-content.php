00f743472e4dab580b78ecd4b9e552c477ad978f
foostart.com <foostart.com@gmail.com>
2019-09-17 16:36:14 +0700

    f

---
+++ 1344/1344-content.php
@@ -0,0 +1,156 @@
+﻿<?php
+    $url_host = 'http://'.$_SERVER['HTTP_HOST'];
+    $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+    $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+    
+    preg_match_all($pattern_uri, __DIR__, $matches);
+    $url_path = $url_host . $matches[1][0];
+    $url_path = str_replace('\\', '/', $url_path);
+?>
+
+    <div class="type-1344">
+        <div class="container">
+            <!--............-->
+            <header class="page-header">
+                <h1>Theme Options</h1>
+                
+            </header>  
+
+            <div class="product-menu">
+                <!--menu-->
+                <div class="Filters " data-parent="olumn-filters">
+                    <ul class=" nav nav-tabs">
+                        
+                        <li ><a data-toggle="tab" href="#a">General Settings</a></li>
+                        <li ><a data-toggle="tab" href="#b">Global Layout</a></li>
+                        <li ><a data-toggle="tab" href="#c">Global Style</a></li>
+                        <li ><a data-toggle="tab" href="#d">Global Typography</a></li>
+                        <li ><a data-toggle="tab" href="#e">Top Bar & Header</a></li>
+                        <li ><a data-toggle="tab" href="#f">Body</a></li>
+                        <li ><a data-toggle="tab" href="#g">Shooter & Subfooter</a></li>
+                        <li ><a data-toggle="tab" href="#h">Site Identiy, Product Archives</a></li>
+                        <li ><a data-toggle="tab" href="#i">Menu, Widget, Static Front Page</a></li>
+                    </ul>
+                </div>
+                <!--..-->
+                <div class="portfolio_wrapper">
+                    <div class="tab-content">
+                        
+                        <div id="a" class="tab-pane fade in active">
+                            <div class="all">
+
+                                <div>
+                                    <div class="image">
+                                        <img class="img-reponsive" src="<?php echo $url_path ?>/image/imageA.png" alt=""/>
+                                        <img class="img-reponsive" src="<?php echo $url_path ?>/image/imageA.png" alt=""/>
+                                        <img class="img-reponsive" src="<?php echo $url_path ?>/image/imageA.png" alt=""/>
+                                        <img class="img-reponsive" src="<?php echo $url_path ?>/image/imageA.png" alt=""/>
+                                        <img class="img-reponsive" src="<?php echo $url_path ?>/image/imageA.png" alt=""/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div id="b" class="tab-pane "style="background: white;" >
+                            <div class="all">
+
+                                <div>
+                                    <div class="image">
+                                        <img class="img-reponsive" src="<?php echo $url_path ?>/image/content.jpg" alt=""/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    
+                        <div id="c" class="tab-pane" style="background: white;">
+                            <div class="all">
+
+                                <div>
+                                    <div class="image">
+                                        <img class="img-reponsive" src="<?php echo $url_path ?>/image/imageC.png" alt=""/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+
+                        <div id="d" class="tab-pane"    style="background: white;">
+                            <div class="all">
+
+                                <div>
+                                    <div class="image">
+                                        <img class="img-reponsive" src="<?php echo $url_path ?>/image/imageD.png" alt=""/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div id="e" class="tab-pane">
+                            <div class="all">
+
+                            <div><h1 style="padding-bottom: 10px"> Top Bar</h1></div>
+                                <div>
+                                    <div class="image">
+                                        <img class="img-reponsive" src="<?php echo $url_path ?>/image/imageE.png" alt=""/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                     
+                        <div id="f" class="tab-pane">
+                            <div class="all">
+
+                                <div>
+                                    <div class="image">
+                                        <img class="img-reponsive" src="<?php echo $url_path ?>/image/imageF.png" alt=""/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div id="g" class="tab-pane">
+                            <div class="all">
+
+                            <div><h1 style="padding-bottom: 10px"> Footer</h1></div>
+
+                                <div>
+                                    <div class="image">
+                                        <img class="img-reponsive" src="<?php echo $url_path ?>/image/imageG.png" alt=""/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div id="h" class="tab-pane">
+                            <div class="all">
+
+                            <div><h1 style="padding-bottom: 10px">Site Identity</h1></div>
+
+                                <div>
+                                    <div class="image">
+                                        <img class="img-reponsive" src="<?php echo $url_path ?>/image/imageH.png" alt=""/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div id="i" class="tab-pane">
+                            <div class="all">
+
+                            <div><h1 style="padding-bottom: 10px"> Menus</h1></div>
+                                <div>
+                                    <div class="image">
+                                        <img class="img-reponsive" src="<?php echo $url_path ?>/image/imageI.png" alt=""/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+         </div>
+     </div>
+
+
+
---
+++ 1346/1346-content.php
@@ -0,0 +1,122 @@
+<?php
+$url_host = $_SERVER['HTTP_HOST'];
+
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+
+preg_match_all($pattern_uri, __DIR__, $matches);
+
+$url_path = $url_host . $matches[1][0];
+
+$url_path = str_replace('\\', '/', $url_path);
+?>
+        <div class="type-1346">
+            <header>
+                <div class="header-top">
+                    <div class="navi-logo">
+                        <a href="#">Délicious</a>
+                    </div>
+                    <div class="navi-menus">
+                        <ul>
+                            <li>
+                                <a href="#">Trang Chủ</a>
+                            </li>
+                            <li>
+                                <a href="#">Chi Tiết</a>
+                                <ul>
+                                    <li><a href="#">Gourmet</a></li>
+                                    <li><a href="#">Coffe</a></li>
+                                    <li><a href="#">Sushi</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#">Thực Đơn</a>
+                                <ul>
+                                    <li><a href="#">Gourmet</a></li>
+                                    <li><a href="#">Coffe</a></li>
+                                    <li><a href="#">Sushi</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#">Dịch Vụ</a>
+                                <ul>
+                                    <li><a href="#">Gourmet</a></li>
+                                    <li><a href="#">Coffe</a></li>
+                                    <li><a href="#">Sushi</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#">Bộ Sưu Tập</a>
+                                <ul>
+                                    <li><a href="#">Gourmet</a></li>
+                                    <li><a href="#">Coffe</a></li>
+                                    <li><a href="#">Sushi</a></li>
+                                </ul>
+                            </li>
+
+                            <li>
+                                <a href="#">Blog</a>
+                                <ul>
+                                    <li><a href="#">Gourmet</a></li>
+                                    <li><a href="#">Coffe</a></li>
+                                    <li><a href="#">Sushi</a></li>
+                                </ul>
+                            </li>
+
+                            <li>
+                                <a href="#">Chức Năng</a>
+                                <ul>
+                                    <li><a href="#">Gourmet</a></li>
+                                    <li><a href="#">Coffe</a></li>
+                                    <li><a href="#">Sushi</a></li>
+                                </ul>
+                            </li>
+
+                            <li>
+                                <a href="#">Shop</a>
+                                <ul>
+                                    <li><a href="#">Gourmet</a></li>
+                                    <li><a href="#">Coffe</a></li>
+                                    <li><a href="#">Sushi</a></li>
+                                </ul>
+                            </li>
+
+                            <li>
+                                <a href="#">Liên Hệ</a>
+                                <ul>
+                                    <li><a href="#">Gourmet</a></li>
+                                    <li><a href="#">Coffe</a></li>
+                                    <li><a href="#">Sushi</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="navi-cart">
+                        <div class="search">
+                            <i class="fa fa-search icon-search"></i>
+                            <form class="search-panel" method="post">
+                                <input type="search" class="menu-search-text" placeholder="Please input search...">
+                            </form>
+                        </div>
+                        <div class="shop-cart">
+                            <a href="#"><i class="fa fa-shopping-bag"></i></a>
+                            <span>0</span>
+                            <div class="list-cart">
+                                <span>Không có sản phẩm nào trong giỏ của bạn.</span>
+                            </div>
+                        </div>
+                        <div id="icon-responsive">
+                            <i class="fa fa-bars"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="header-banner">
+                    <div class="banner-text">
+                        <h2>Thực Đơn</h2>
+                        <hr>
+                        <p life>Cuộc sống tốt hơn với Délicious</p>
+                    </div>
+                </div>
+            </header>
+        </div>
